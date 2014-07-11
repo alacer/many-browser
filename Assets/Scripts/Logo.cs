87b4695dcb6cadcf37834147accf7566de6d8e68
Lance Hughes <thisislance@yahoo.com>
2014-07-11 15:28:34 -0700

    fixed helix stickiness

---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		yield return new WaitForSeconds(3);
+
+		LeanTween.alpha(gameObject,0, 2);
+	
+	}
+	
+
+}
